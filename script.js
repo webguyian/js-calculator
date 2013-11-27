42114068e9d3432395536611703a822bb7bb3a54
Ian Mac <webguyian@gmail.com>
2013-11-26 22:21:38 -0500

    Add keypress events

--- script.js
+++ script.js
@@ -5,21 +5,21 @@
 
   // Set variables
   var doc = document,
-    $ = doc.querySelector.bind(doc),
-    $$ = doc.querySelectorAll.bind(doc),
-    calcWindow = $(".calc-window"),
-    digitBtns = $$(".btn-digit"),
-    methodBtns = $$(".btn-method"),
-    percentBtn = $('.btn-percent'),
-    clearBtn = $(".btn-clear"),
-    equalBtn = $(".btn-equals"),
-    currentExp = "",
-    numBuilder = "";
+  $ = doc.querySelector.bind(doc),
+  $$ = doc.querySelectorAll.bind(doc),
+  calcWindow = $(".calc-window"),
+  digitBtns = $$(".btn-digit"),
+  methodBtns = $$(".btn-method"),
+  percentBtn = $('.btn-percent'),
+  clearBtn = $(".btn-clear"),
+  equalBtn = $(".btn-equals"),
+  currentExp = "",
+  numBuilder = "";
   
   // Create event listener function
   function addEventListenerList(list, event, fn) {
     var i,
-        len = list.length;
+    len = list.length;
     for (i = 0; i < len; i++) {
       list[i].addEventListener(event, fn, false);
     }
@@ -34,15 +34,16 @@
   // Sets current expression
   function setExp(exp) {
     currentExp = exp;
+    return currentExp;
   }
   
   // Gets current expression
-  function getExp(){
+  function getExp() {
     return currentExp;
   }
 
   // Displays expression in window
-  function display(input){
+  function display(input) {
     calcWindow.innerHTML = input;
   }
 
@@ -70,7 +71,7 @@
   // Calculates percent
   function calculatePercent() {
     var num = getExp(),
-        percent = (num / 100);
+    percent = (num / 100);
     setExp(percent);
     display(percent);
     return percent;
@@ -79,14 +80,24 @@
   // Evaluates current expression
   function calculate(exp) {
   	var num = eval(exp),
+    calculatedVal;
     calculatedVal = Math.round(num * 100) / 100;
-  	return calculatedVal;
+    return calculatedVal;
   }
 
   // Add digit to expression
   function touchBtn() {
     var thisNum = getValue(this);
-        currentExp += thisNum;
+    currentExp += thisNum;
+    numBuilder += thisNum;
+    display(numBuilder);
+    return currentExp;
+  }
+
+  function keyBtn() {
+    var thisNum = getKey();
+    console.log(thisNum);
+    currentExp += thisNum;
     numBuilder += thisNum;
     display(numBuilder);
     return currentExp;
@@ -104,15 +115,57 @@
   function reset() {
   	currentExp = "";
     numBuilder = "";
-  	display(0);
+    display(0);
   }
-  
-  // Attach event listeners to buttons
+
+  // Handle key press
+  function getKey(e) { 
+    e = e || window.event; 
+    var charCode = e.keyCode || e.charCode, 
+    keyPressed = String.fromCharCode(charCode);
+    if (charCode >= 48 && charCode <= 57) {
+      // Digit
+      currentExp += keyPressed;
+      numBuilder += keyPressed;
+      display(numBuilder);
+      setExp(currentExp);
+    } else if (charCode > 41 && charCode < 48) {
+      // Symbol
+      var thisMethod = keyPressed,
+          separatedExp = "(" + currentExp + ")";
+        numBuilder = "";
+    currentExp = separatedExp + thisMethod;
+    setExp(currentExp);
+    return currentExp;
+    } else {
+     return false;
+   }
+ };
+
+ // Handle special keys
+ function specialKeys(evt) {
+  evt = evt || window.event;
+  var charCode = evt.keyCode || evt.charCode,
+  keyPressed = String.fromCharCode(charCode),
+  currentExp = getExp();
+  if (charCode == 8 || charCode == 46) {
+    // 'Delete' or 'Backspace' key
+    evt.preventDefault();
+    reset();
+  } else if (charCode === 13) {
+    // 'Enter' key
+    evaluateExp();
+  }
+}
+
+  // Attach event listeners
   addEventListenerList(digitBtns, 'click', touchBtn);
   addEventListenerList(methodBtns, 'click', methodBtn);
   percentBtn.addEventListener('click', calculatePercent, false);
   equalBtn.addEventListener('click', evaluateExp, false);
   clearBtn.addEventListener('click', reset, false);
+  doc.addEventListener('keypress', getKey, false);
+  doc.addEventListener('keydown', specialKeys, false);
 
   // Set initial display
   display(0);
